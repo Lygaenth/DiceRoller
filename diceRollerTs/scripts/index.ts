9f3d29ffef8f308b9c73ea4a56a938bbfd913b06
Lygaenth <115181383+Lygaenth@users.noreply.github.com>
2023-09-11 17:53:07 +0200

    Cleaning solution from test projects

--- diceRollerTs/scripts/index.ts
+++
@@ -1,9 +0,0 @@
-﻿import * as signalR from "@microsoft/signalr";
-import "./../css/main.css";
-
-const connection = new signalR.HubConnectionBuilder()
-    .withUrl("/hub")
-    .build();
-
-connection.start().catch((err) => document.write(err));
-
--- diceRollerTs/wwwroot/js/index.js
+++
@@ -1,7 +0,0 @@
-//import * as signalR from "@microsoft/signalr";
-//import "./../css/main.css";
-//const connection = new signalR.HubConnectionBuilder()
-//    .withUrl("/hub")
-//    .build();
-//connection.start().catch((err) => document.write(err));
-//# sourceMappingURL=index.js.map
